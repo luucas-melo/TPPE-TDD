78cfbf7cb444bcd5fcd51f5a6086b7f176d20d8e
luucas-melo <lucasmelodos322@gmail.com>
2022-07-11 17:25:41 -0300

    Falsificao: Acesso hora cheia
    Co-authored-by: AlineLermen <aline.helena.lermen@gmail.com>
    Co-authored-by: NilvanPeres <nilvanperes@gmail.com>
    Co-authored-by: NiloMendonca <nilojrmendonca@gmail.com>
    Co-authored-by: JoaoVitorFarias <jvlopesfarias@gmail.com>

---
+++ estacionamento-tdd/src/Main/Estacionamento.java
@@ -0,0 +1,26 @@
+package Main;
+
+public class Estacionamento {
+	
+	String estacionamento;
+	float valorFracao,  valorHoraCheia;
+	
+	public void cadastraAcesso() {
+		
+		
+	}
+	
+	public void cadastraEstacionamento(String nomeEstacionamento, float valorFracao, float valorHoraCheia) {
+		this.estacionamento = nomeEstacionamento;
+		this.valorFracao = valorFracao;
+		this.valorHoraCheia = valorHoraCheia;
+	}
+	
+	public int calculaAcesso(String estacionamentoParaAcessso, String entrada, String saida) {
+		
+		return 60;
+		
+	}
+
+	
+}
---
+++ estacionamento-tdd/src/Tests/TestEstacionamento.java
@@ -0,0 +1,31 @@
+package Tests;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Main.Estacionamento;
+
+
+
+public class TestEstacionamento {
+	
+	Estacionamento estacionamento;
+	
+	
+	@Before
+	public void setup() {
+		estacionamento = new Estacionamento();
+		estacionamento.cadastraEstacionamento("Estacionamento1", 30f, 0.15f);
+	}
+	
+	@Test
+	public void calculaValorAcessoHoraCheia() {
+		int valorAcesso = estacionamento.calculaAcesso("Estacionamento1", "8:30", "9:30");
+		assertEquals(valorAcesso, 60);
+		
+	}
+
+}
