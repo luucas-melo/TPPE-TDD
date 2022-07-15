c204ec97255758717c74f23f5090359a816240f8
Nilo Mendonça <nilojrmendonca@gmail.com>
2022-07-15 15:44:02 -0300

    WIP: Correção de erro com 'LocalDateTime'
    Co-authored-by: NilvanPeres <nilvanperes@gmail.com>

--- estacionamento-tdd/src/Tests/TesteAcessoNoturno.java
+++ estacionamento-tdd/src/Tests/TesteAcessoNoturno.java
@@ -13,20 +13,20 @@ public class TesteAcessoNoturno {
 	
 	@Before
 	public void setup() {
-		estacionamento = new Estacionamento("Estacionamento2", 20f, 0.10f, 70f, 0.30f, "21:00", "07:00");	
+		estacionamento = new Estacionamento("Estacionamento2", 20f, 0.10f, 70f, 0.30f, "15-08-2022 19:00:00", "15-08-2022 08:00:00");	
 	}
 	
 	@Test
 	public void calculaValorAcessoNoturno1() {
-		Acesso acesso = new Acesso(estacionamento, "RM3A9", "21:36", "06:12");
+		Acesso acesso = new Acesso(estacionamento, "RM3A9", "15-08-2022 21:36:00", "15-08-2022 06:12:00");
 		float valorAcesso = acesso.calculaAcesso();
 		assertEquals(valorAcesso, 21f, 0.1f);
 	}
 	
 	@Test
 	public void calculaValorAcessoNoturno2() {
-		Acesso acesso = new Acesso(estacionamento, "RM3A9", "00:40", "05:40");
+		Acesso acesso = new Acesso(estacionamento, "RM3A9", "15-08-2022 00:40:00", "15-08-2022 05:40:00");
 		float valorAcesso = acesso.calculaAcesso();
-		assertEquals(valorAcesso, 21f, 0.1f);
+		assertEquals(valorAcesso, 360f, 0.1f);
 	}
 }
