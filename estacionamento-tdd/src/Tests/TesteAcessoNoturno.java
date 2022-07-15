327730f8e6bcd0445876a080bba1f7c01e524acd
Nilo Mendonça <nilojrmendonca@gmail.com>
2022-07-15 17:01:49 -0300

    Triangulação e Parametrização: Adicionada triangulação e parametrização para o cálculo de acesso para acesso noturno
    Co-authored-by: NilvanPeres <nilvanperes@gmail.com>

--- estacionamento-tdd/src/Tests/TesteAcessoNoturno.java
+++ estacionamento-tdd/src/Tests/TesteAcessoNoturno.java
@@ -1,34 +1,74 @@
 package Tests;
 
+
 import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import Main.Acesso;
 import Main.Estacionamento;
 
+
+@RunWith(Parameterized.class)
 public class TesteAcessoNoturno {
-	Estacionamento estacionamento1, estacionamento2, estacionamento3;
+	
+	private String nome;
+	private float valorFracao, valorHoraCheia, valorDiariaDiurna;
+	private float porcentagemDiariaNoturna;
+	private String entradaNoturna, retiradaNoturna;
+	
+	private Estacionamento estacionamento;
+	private String placa, horaEntrada, horaSaida;
+	private float valorCalculadoAcesso;
+	private Acesso acesso;
 	
 	@Before
 	public void setup() {
-		estacionamento1 = new Estacionamento("Estacionamento1", 30f, 0.15f, 120f, 0.45f, "15-08-2022 19:00:00", "15-08-2022 08:00:00");
-		estacionamento2 = new Estacionamento("Estacionamento2", 20f, 0.10f, 70f, 0.30f, "15-08-2022 21:00:00", "15-08-2022 07:00:00");	
-		//estacionamento3 = new Estacionamento("Estacionamento3", 10f, 0f, 50f, 0.40f, "15-08-2022 20:00:00", "15-08-2022 08:00:00");	
+		estacionamento = new Estacionamento(nome, valorFracao, valorHoraCheia, 
+				valorDiariaDiurna, porcentagemDiariaNoturna, entradaNoturna, retiradaNoturna);
+		acesso = new Acesso(estacionamento, placa, horaEntrada, horaSaida);
+	}
+	
+	public TesteAcessoNoturno(String nomeEstacionamento, float valorFracao, float valorHoraCheia, 
+			float valorDiariaDiurna, float porcentagemDiariaNoturna,  
+			String entradaNoturna, String retiradaNoturna, String placa, String horaEntrada, 
+			String horaSaida, float valorCalculadoAcesso) {
+		this.nome = nomeEstacionamento;
+		this.valorFracao = valorFracao;
+		this.valorHoraCheia = valorHoraCheia;
+		this.valorDiariaDiurna = valorDiariaDiurna;
+		this.porcentagemDiariaNoturna = porcentagemDiariaNoturna;
+		this.entradaNoturna =  entradaNoturna;
+		this.retiradaNoturna = retiradaNoturna;
+		
+		this.placa = placa; 
+		this.horaEntrada = horaEntrada; 
+		this.horaSaida = horaSaida;
+		this.valorCalculadoAcesso = valorCalculadoAcesso;
+	}
+	
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[][] resposta = new Object[][] {
+			{"Estacionamento1", 30f, 0.15f, 120f, 0.45f, "15-08-2022 19:00:00", "15-08-2022 08:00:00", "RM3A9", "15-08-2022 19:00:00", "16-08-2022 08:00:00", 54f}, 
+			{"Estacionamento2", 20f, 0.10f, 70f, 0.30f, "15-08-2022 21:00:00", "15-08-2022 07:00:00", "RM3A9", "15-08-2022 21:36:00", "16-08-2022 06:12:00", 21f},
+			{"Estacionamento3", 10f, 0f, 50f, 0.40f, "15-08-2022 20:00:00", "15-08-2022 08:00:00", "RM3A9", "15-08-2022 20:00:00", "16-08-2022 08:00:00", 20f}
+		};
+		
+		return Arrays.asList(resposta);
 	}
 	
 	@Test
-	public void calculaValorAcessoNoturno1() {
-		Acesso acesso = new Acesso(estacionamento2, "RM3A9", "15-08-2022 21:36:00", "15-08-2022 06:12:00");
-		float valorAcesso = acesso.calculaAcesso();
-		assertEquals(valorAcesso, 21f, 0.1f);
-	}
-	
-	@Test
-	public void calculaValorAcessoNoturno2() {
-		Acesso acesso = new Acesso(estacionamento1, "RM3A9", "15-08-2022 19:00:00", "15-08-2022 08:00:00");
-		float valorAcesso = acesso.calculaAcesso();
-		assertEquals(valorAcesso, 54f, 0.1f);
+	public void calculaValorAcessoNoturno() {
+		
+		float valorAcesso = this.acesso.calculaAcesso();
+		assertEquals(valorCalculadoAcesso, valorAcesso, 0.1f);
 	}
 }
+
