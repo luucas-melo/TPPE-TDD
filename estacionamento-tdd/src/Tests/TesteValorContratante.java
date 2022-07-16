67ef4ee1e604425b3417c5434e823f493777917e
JoaoVitorFarias <jvlopesfarias@gmail.com>
2022-07-16 17:25:59 -0300

    Triangulação: Parametrização do valor contratante

--- estacionamento-tdd/src/Tests/TesteValorContratante.java
+++ estacionamento-tdd/src/Tests/TesteValorContratante.java
@@ -3,77 +3,95 @@ package Tests;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import Main.Acesso;
 import Main.Estacionamento;
 
+@RunWith(Parameterized.class)
 public class TesteValorContratante {
 
 	private Estacionamento estacionamento;
-	private Estacionamento estacionamento2;
-	private Estacionamento estacionamento3;
+	Object[][] acessos;
+	private String nomeEstacionamento, entradaNoturna, retiradaNoturna;
+	private float valorFracao, valorHoraCheia, valorDiariaDiurna, porcentagemDiariaNoturna, valorAcessoMensalista,
+	valorAcessoEvento, retornoContratante, totalContratante;
+	private int capacidade;
+	
+	public TesteValorContratante(String nomeEstacionamento, float valorFracao, float valorHoraCheia, float valorDiariaDiurna, 
+			float porcentagemDiariaNoturna,  String entradaNoturna, String retiradaNoturna, float valorAcessoMensalista, 
+			float valorAcessoEvento, int capacidade, float retornoContratante, Object[][] acessos, float totalContratante) {
+		
+		this.nomeEstacionamento = nomeEstacionamento;
+		this.valorFracao = valorFracao;
+		this.valorHoraCheia = valorHoraCheia;
+		this.valorDiariaDiurna = valorDiariaDiurna;
+		this.porcentagemDiariaNoturna = porcentagemDiariaNoturna;
+		this.entradaNoturna =  entradaNoturna;
+		this.retiradaNoturna = retiradaNoturna;
+		this.valorAcessoMensalista = valorAcessoMensalista;
+		this.valorAcessoEvento = valorAcessoEvento;
+		this.capacidade = capacidade;
+		this.retornoContratante = retornoContratante;
+		
+		this.acessos = acessos;
+		this.totalContratante = totalContratante;
+	}
+	
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[] acesso1 = new Object[] {"AM31J", "", "", "evento"};
+		Object[] acesso2 = new Object[] {"AM31J", "15-08-2022 08:00:00", "15-08-2022 18:00:00", ""};
+		Object[] acesso3 = new Object[] {"AM31J", "15-08-2022 10:00:00", "15-08-2022 10:43:00", ""};
+		
+		Object[][] resposta = new Object[][] {
+			{"Estacionamento1", 30f, 0.15f, 120f, 0.45f, "15-08-2022 19:00:00", "15-08-2022 08:00:00", 600f, 50f, 300, 0.5f, 
+				new Object[][]{
+					acesso1,
+					acesso2,
+					acesso3},130f}, 
+			{"Estacionamento2", 20f, 0.15f, 120f, 0.45f,"15-08-2022 21:00:00", "15-08-2022 07:00:00", 455f, 60f, 120, 0.6f, 
+				new Object[][]{
+					acesso1,
+					acesso2,
+					acesso3},144f},
+			{"Estacionamento3", 10f, 0f, 50f, 0.40f, "15-08-2022 20:00:00", "15-08-2022 08:00:00", 350f, 40f, 600, 0.7f,
+				new Object[][]{
+					acesso1,
+					acesso2,
+					acesso3},84f},
+		};
+		
+		return Arrays.asList(resposta);
+	}
+	
 	
 	@Before
 	public void setup() {
-		estacionamento = new Estacionamento("Estacionamento1", 30f, 0.15f, 120f, 0.45f, 
-				"15-08-2022 19:00:00", "15-08-2022 08:00:00", 600f, 50f, 300, 0.5f);
-		estacionamento2 = new Estacionamento("Estacionamento2", 20f, 0.15f, 120f, 0.45f, 
-				"15-08-2022 21:00:00", "15-08-2022 07:00:00", 455f, 60f, 120, 0.6f);
-		estacionamento3 = new Estacionamento("Estacionamento3", 10f, 0f, 50f, 0.40f, 
-				"15-08-2022 20:00:00", "15-08-2022 08:00:00", 350f, 40f, 600, 0.7f);
+		
+		estacionamento = new Estacionamento (nomeEstacionamento, valorFracao, valorHoraCheia,valorDiariaDiurna,
+				porcentagemDiariaNoturna, entradaNoturna, retiradaNoturna, valorAcessoMensalista, valorAcessoEvento,
+				capacidade,retornoContratante);
 	}
 	
 	@Test
 	public void calculaValorContratante() {
 		
-		Acesso acesso1 = new Acesso(estacionamento, "AM31J", "", "", "evento");
-		Acesso acesso2 = new Acesso(estacionamento, "AM31J", "15-08-2022 08:00:00", "15-08-2022 18:00:00", "");
-		Acesso acesso3 = new Acesso(estacionamento, "AM31J", "15-08-2022 10:00:00", "15-08-2022 10:43:00", "");
-		
 		ArrayList<Acesso> listaAcessos = new ArrayList<>();
-		listaAcessos.add(acesso1);
-		listaAcessos.add(acesso2);
-		listaAcessos.add(acesso3);
+		
+		for (Object[] acesso: acessos) {
+			listaAcessos.add(new Acesso(this.estacionamento, (String)acesso[0], 
+							(String)acesso[1], (String)acesso[2], (String)acesso[3]));
+		}
+		
 		
 		float valorContratante = this.estacionamento.calculaValorContratante(listaAcessos);
-		System.out.print(valorContratante);
-		assertEquals(valorContratante, 130f, 0.1f);
-	}
-	
-	@Test
-	public void calculaValorContratante2() {
-		
-		Acesso acesso1 = new Acesso(estacionamento2, "AM31J", "", "", "evento");
-		Acesso acesso2 = new Acesso(estacionamento2, "AM31J", "15-08-2022 08:00:00", "15-08-2022 18:00:00", "");
-		Acesso acesso3 = new Acesso(estacionamento2, "AM31J", "15-08-2022 10:00:00", "15-08-2022 10:43:00", "");
-		
-		ArrayList<Acesso> listaAcessos = new ArrayList<>();
-		listaAcessos.add(acesso1);
-		listaAcessos.add(acesso2);
-		listaAcessos.add(acesso3);
-		
-		float valorContratante = this.estacionamento2.calculaValorContratante(listaAcessos);
-		System.out.print(valorContratante);
-		assertEquals(valorContratante, 144f, 0.1f);
-	}
-	
-	@Test
-	public void calculaValorContratante3() {
-		
-		Acesso acesso1 = new Acesso(estacionamento3, "AM31J", "", "", "evento");
-		Acesso acesso2 = new Acesso(estacionamento3, "AM31J", "15-08-2022 08:00:00", "15-08-2022 18:00:00", "");
-		Acesso acesso3 = new Acesso(estacionamento3, "AM31J", "15-08-2022 10:00:00", "15-08-2022 10:43:00", "");
-		
-		ArrayList<Acesso> listaAcessos = new ArrayList<>();
-		listaAcessos.add(acesso1);
-		listaAcessos.add(acesso2);
-		listaAcessos.add(acesso3);
-		
-		float valorContratante = this.estacionamento3.calculaValorContratante(listaAcessos);
-		System.out.print(valorContratante);
-		assertEquals(valorContratante, 84f, 0.1f);
+		assertEquals(valorContratante, this.totalContratante, 0.1f);
 	}
 }
